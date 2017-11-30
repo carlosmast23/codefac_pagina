bebe78ee6c0b272f995e9ba0b9b6bf6e1e644e6f
jc100pl2017 <juancarlos100pl@hotmail.com>
2017-11-30 12:39:24 -0500

    Webservice with encoded

--- application/controllers/Mysoapserver.php
+++ application/controllers/Mysoapserver.php
@@ -9,13 +9,13 @@ class Mysoapserver extends MY_Controller {
         $ns=base_url()."mysoapserver";
         $this->load->library("Nusoap_lib"); //load the library here
         $this->nusoap_server = new soap_server();
-        $this->nusoap_server->configureWSDL("SOAPServer", $ns,false,"document");
+        $this->nusoap_server->configureWSDL("SOAPServer", $ns);
         //$this->nusoap_server->wsdl->schemaTargetNamespace = $ns;
 
         //registrando funciones
         $input_array = array ('a' => "xsd:string", 'b' => "xsd:string");
         $return_array = array ("return" => "xsd:string");
-        $this->nusoap_server->register('addnumbers', $input_array, $return_array, "urn:SOAPServerWSDL", "urn:".$ns."/addnumbers", "document", "literal", "Addition Of Two Numbers");
+        $this->nusoap_server->register('addnumbers', $input_array, $return_array, "urn:SOAPServerWSDL", "urn:".$ns."/addnumbers", "rpc", "encoded", "Addition Of Two Numbers");
     }
 
     function index()
