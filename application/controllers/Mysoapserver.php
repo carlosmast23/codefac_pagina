bad6a299313d5225faafc5ec0385f9f9144320f8
jc100pl2017 <juancarlos100pl@hotmail.com>
2017-11-30 10:34:16 -0500

    Webservice actualizaccion bionding document

--- application/controllers/Mysoapserver.php
+++ application/controllers/Mysoapserver.php
@@ -6,34 +6,26 @@ class Mysoapserver extends MY_Controller {
     function __construct() {
         parent::__construct();
         //$this->load->model(''); //load your models here
-
+        $ns=base_url()."mysoapserver";
         $this->load->library("Nusoap_lib"); //load the library here
         $this->nusoap_server = new soap_server();
-        $this->nusoap_server->configureWSDL("Mysoapserver", "urn:Mysoapserver");
-        $this->nusoap_server->register(
-            "login", array("usuario" => "xsd:string","clave" => "xsd:string"), array("return" => "xsd:string"), "urn:Mysoapserver", "urn:Mysoapserver#login", "rpc", "encoded", "Login WS"
-            );
+        $this->nusoap_server->configureWSDL("SOAP Server", $ns,false,"document");
+        $this->nusoap_server->wsdl->schemaTargetNamespace = $ns;
 
-
+        //registrando funciones
+        $input_array = array ('a' => "xsd:string", 'b' => "xsd:string");
+        $return_array = array ("return" => "xsd:string");
+        $this->nusoap_server->register('addnumbers', $input_array, $return_array, "urn:SOAPServerWSDL", "urn:".$ns."/addnumbers", "document", "encoded", "Addition Of Two Numbers");
     }
 
-    function index() {
-
-
-        function login($usuario,$clave) {
-          $ci =& get_instance();
-            /*if (!$tmp) {
-                return new soap_fault('-1', 'Server', 'Parameters missing for echoTest().', 'Please refer documentation.');
-            } else {
-                return "from MySoapServer() : $tmp";
-            }*/
-
-            if($usuario=="admin" && $clave=="admin123")
-                return "success";
-            else
-                return "fail";
+    function index()
+    {
+        function addnumbers($a,$b)
+        {
+            $c = $a + $b;
+            return $c;
         }
-        
-        $this->nusoap_server->service(file_get_contents("php://input")); //shows the standard info about service
+
+        $this->nusoap_server->service(file_get_contents("php://input"));
     }
 }
