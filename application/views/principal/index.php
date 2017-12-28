0f75bfaeb8a5d1dd41e6810af4578566f5b0a0ab
jc100pl2017 <juancarlos100pl@hotmail.com>
2017-12-28 10:34:42 -0500

    actualizacion precios

--- application/views/principal/index.php
+++ application/views/principal/index.php
@@ -1,65 +1,65 @@
 
 <div class="container">
 
-    <div class="col-md-12">
-      <div class="col-md-7">
-        <h2 class="text-center titulo_principal">CODEFACT - Facturación Electrónica</h2>
+  <div class="col-md-12">
+    <div class="col-md-7">
+      <h2 class="text-center titulo_principal">CODEFACT - Facturación Electrónica</h2>
 
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-          <!-- Indicators -->
-          <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-          </ol>
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
 
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner">
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
 
-            <div class="item active">
-              <img class="img-responsive" src="<?=base_url()?>img/f1.png" />
-              <div class="carousel-caption">
-                <h3>FACTURACIÓN ELECTRÓNICA GRATUITA</h3>
-                <p><a href="<?=base_url()?>general/registro" class="btn btn-default">¡Compra ya!</a></p>
-              </div>
+          <div class="item active">
+            <img class="img-responsive" src="<?=base_url()?>img/f1.png" />
+            <div class="carousel-caption">
+              <h3>FACTURACIÓN ELECTRÓNICA GRATUITA</h3>
+              <p><a href="<?=base_url()?>general/registro" class="btn btn-default">¡Compra ya!</a></p>
             </div>
-
-            <div class="item">
-              <img class="img-responsive" src="<?=base_url()?>img/f2.png" />
-              <div class="carousel-caption">
-                <h3>Facturación para pequeña y mediana empresa</h3>
-                <p><a href="<?=base_url()?>general/registro" class="btn btn-default">Más información</a></p>
-              </div>
-            </div>
-
-            <div class="item">
-              <img class="img-responsive" src="<?=base_url()?>img/f3.png" />
-              <div class="carousel-caption">
-                <h3>LLeva el control de tus facturas online</h3>
-                <p><a href="<?=base_url()?>general/registro" class="btn btn-default">Regístrate</a></p>
-              </div>
-            </div>
-
           </div>
 
-          <!-- Left and right controls -->
-          <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
-      </div>
-      <div class="col-md-5">
-        <h3>
-        El 2018 es el plazo para emitir comprobantes electrónicos bajo el esquema ‘Off Line’ ...
-       </h3>        <a target="_blank" href="http://www.elcomercio.com/actualidad/plazo-comprobanteselectronicos-offline-sri-facturas.html">Ver Noticia.</a>
+          <div class="item">
+            <img class="img-responsive" src="<?=base_url()?>img/f2.png" />
+            <div class="carousel-caption">
+              <h3>Facturación para pequeña y mediana empresa</h3>
+              <p><a href="<?=base_url()?>general/registro" class="btn btn-default">Más información</a></p>
+            </div>
+          </div>
 
-       <h3 class="titulo_principal" id="caracteristicas">Características</h3>
-       <ul class="blanco">
+          <div class="item">
+            <img class="img-responsive" src="<?=base_url()?>img/f3.png" />
+            <div class="carousel-caption">
+              <h3>LLeva el control de tus facturas online</h3>
+              <p><a href="<?=base_url()?>general/registro" class="btn btn-default">Regístrate</a></p>
+            </div>
+          </div>
+
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-5">
+      <h3>
+        El 2018 es el plazo para emitir comprobantes electrónicos bajo el esquema ‘Off Line’ ...
+      </h3>        <a target="_blank" href="http://www.elcomercio.com/actualidad/plazo-comprobanteselectronicos-offline-sri-facturas.html">Ver Noticia.</a>
+
+      <h3 class="titulo_principal" id="caracteristicas">Características</h3>
+      <ul class="blanco">
         <li>Emisión de Facturas.</li>
         <li>Notas de crédito y débito.</li>
         <li>Administración de Clientes.</li>
@@ -67,7 +67,7 @@
         <li>Informes.</li>
       </ul>
       <h3 class="titulo_principal">Beneficios</h3>
-       <ul class="blanco">
+      <ul class="blanco">
         <li>Información eficiente y de calidad</li>
         <li>Ahorro de costes de impresión, papel, envío postal y almacenamiento.</li>
         <li>Eliminación de espacio físico para archivo. El archivo electrónico es más ágil y seguro.</li>
@@ -87,25 +87,36 @@
       <tr>
         <th>FUNCIONALIDADES</th>
         <th>CODEFAC-LITE</th>
+        <th>CODEFAC-LITE PRO</th>
         <th>CODEFAC-PRO</th>
       </tr>
       <tr>
         <td>ANUAL</td>
         <td>1000 facturas</td>
-        <td>2500 facturas</td>
+        <td>ilimitado</td>
+        <td>ilimitado</td>
       </tr>
-      <tr>
-        <td>Ambiente</td>
-        <td>Escritorio</td>
-        <td>Web</td>
-      </tr>  
       <tr>
         <td>Número de máquinas</td>
         <td>1</td>
+        <td>1</td>
+        <td>ilimitado</td>
+      </tr>
+      <tr>
+        <td>Número de usuarios</td>
+        <td>1</td>
+        <td>ilimitado</td>
         <td>ilimitado</td>
       </tr> 
       <tr>
+        <td>Ambiente</td>
+        <td>escritorio</td>
+        <td>escritorio</td>
+        <td>web</td>
+      </tr>
+      <tr>
         <td>Notas de débito y crédito</td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
       </tr> 
@@ -113,61 +124,92 @@
         <td>Administración de clientes</td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
       </tr>
       <tr>
         <td>Administración de productos o servicios</td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
       </tr>
       <tr>
         <td>Administración de proveedores – distribuidores</td>
         <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
       </tr>
       <tr>
         <td>Administración de ordenes de trabajo</td>
+        <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
       </tr>
       <tr>
         <td>Inventario</td>
         <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
       </tr> 
       <tr>
         <td>Control de descuentos y promociones</td>
+        <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
       </tr> 
       <tr>
         <td>Venta de crédito</td>
         <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
       </tr> 
       <tr>
         <td>Utilidades adicionales (notas, calendario, ventas diarias)</td>
+        <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
       </tr>  
       <tr>
         <td>Respaldos programados</td>
         <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
-        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
-      </tr> 
-      <tr>
-        <td>Informes</td>
-        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
-        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
-      </tr>
-      <tr>
-        <td>Soporte técnico 1 año</td>
         <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
         <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
       </tr>
       <tr>
+        <td>Informes</td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+      </tr>
+      <tr>
+        <td>Soporte técnico 1 año (email)</td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+      </tr>
+      <tr>
+        <td>Incorporación logo</td>
+        <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+      </tr>
+      <tr>
+        <td>Incorporación de colores empresariales</td>
+        <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+      </tr>
+      <tr>
+        <td>Diponibilidad de actualizaciones</td>
+        <td> <img src="<?=base_url()?>img/icon_okn.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+        <td> <img src="<?=base_url()?>img/icon_ok.png" height="20"/> </td>
+      </tr>   
+
+      <tr>
         <td>PRECIO / ANUAL</td>
         <td><span class="precio">$ 0 </span></td>
-        <td><span class="precio">$ 249 <span class="supidc">.99</span> </td>
+        <td><span class="precio">$ 159 </span> <span class="supidc">.99</span></td>
+        <td><span class="precio">$ 249 </span> <span class="supidc">.99</span> </td>
       </tr>
     </table>
 
