1636167c8446595be5857f3c6786657c488c5f32
jc100pl2017 <juancarlos100pl@hotmail.com>
2018-01-29 15:23:34 -0500

    actualizacion registro

--- application/models/General_model.php
+++ application/models/General_model.php
@@ -40,6 +40,10 @@ class General_model extends CI_Model {
      $this->db->insert("proveedores",$data);
      
      $id=$this->db->insert_id();
+
+     var_dump($data)
+     die("hasta aca como leva ");
+     
      $enlace= $this->google->codificar_parametro("general/verificacion/",$id);
 
      $this->email_model->enviar_mail($email,"Verificación Codefac","Valida tu cuenta en el siguiente enlace $enlace. Gracias por registrarte con nosotros. ");
