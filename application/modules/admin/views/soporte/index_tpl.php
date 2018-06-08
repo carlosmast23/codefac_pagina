6edc4c1c144ce7bb9e252de4c41c8cb3af70dc73
jc100pl2017 <juancarlos100pl@hotmail.com>
2018-06-08 11:15:08 -0500

    clave hash para usuarios soporte

--- application/modules/admin/views/soporte/index_tpl.php
+++ application/modules/admin/views/soporte/index_tpl.php
@@ -2,4 +2,8 @@
 	<td>{sop_usuario}</td>
 	<td>{sop_fechavisita}</td>
 	<td>{sop_estado}</td>
+	<td>
+		<a class="btn btn-primary" href="<?=base_url()?>admin/soporte/modificar/{id}">Modificar</a>
+		<a class="btn btn-primary" href="<?=base_url()?>admin/soporte/eliminar/{id}">Eliminar</a>
+	</td>
 </tr>
