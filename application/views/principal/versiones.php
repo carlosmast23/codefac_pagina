ad406dcf6e6126ad76ef2eeb48303d875726dcfa
jc100pl2017 <juancarlos100pl@hotmail.com>
2018-07-03 15:07:29 -0500

    Subido archivo de version 1.2.4 y su vista en pagina

--- application/views/principal/versiones.php
+++ application/views/principal/versiones.php
@@ -4,7 +4,7 @@
 		Las versiones estables se publican despues de haber sido auditadas y probadas. Versiones anteriores no pueden contener ciertas funcionalidades o actualizaciones que conllevan las ultimas versiones del sistema.
 	</div>
 	<div class="list-group">
-		<a href="<?=base_url()?>instalador/avispa.png" class="list-group-item active" rel="contd">
+		<a href="<?=base_url()?>instalador/codefactlite1.2.4.rar" class="list-group-item active" rel="contd">
 			<h4 class="list-group-item-heading"><span class="glyphicon glyphicon-save"></span> Codefac Lite 1.2.4 </h4>
 			<p class="list-group-item-text">Versión estable. Descargas: <span class="badge" id="nv"><?=$numd?></span> </p>
 		</a>
@@ -22,9 +22,9 @@
 		</a>
 	</div>
 	<div class="list-group">
-		<a href="<?=base_url()?>instalador/avispa.png" class="list-group-item">
+		<a href="<?=base_url()?>instalador/codefactlite1.2.4.rar" class="list-group-item">
 			<h4 class="list-group-item-heading"><span class="glyphicon glyphicon-save"></span> Codefac Lite 1.2.4</h4>
-			<p class="list-group-item-text">Compatible con Windows  - 2018-07-03</p>
+			<p class="list-group-item-text">Compatible con Windows  - 2018-03-07</p>
 		</a>
 	</div>
 
