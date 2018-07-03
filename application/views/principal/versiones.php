e9873a85a0927384875cafe32cb9ef26c86c41c7
jc100pl2017 <juancarlos100pl@hotmail.com>
2018-07-03 11:02:12 -0500

    Agregado campo para colocar numera version Codefac Lite 1.2.4

--- application/views/principal/versiones.php
+++ application/views/principal/versiones.php
@@ -4,8 +4,8 @@
 		Las versiones estables se publican despues de haber sido auditadas y probadas. Versiones anteriores no pueden contener ciertas funcionalidades o actualizaciones que conllevan las ultimas versiones del sistema.
 	</div>
 	<div class="list-group">
-		<a href="<?=base_url()?>instalador/codefactlite1.0.rar" class="list-group-item active" rel="contd">
-			<h4 class="list-group-item-heading"><span class="glyphicon glyphicon-save"></span> Codefac Lite 1.0 </h4>
+		<a href="<?=base_url()?>instalador/avispa.png" class="list-group-item active" rel="contd">
+			<h4 class="list-group-item-heading"><span class="glyphicon glyphicon-save"></span> Codefac Lite 1.2.4 </h4>
 			<p class="list-group-item-text">Versión estable. Descargas: <span class="badge" id="nv"><?=$numd?></span> </p>
 		</a>
 	</div>
@@ -19,6 +19,12 @@
 		<a href="<?=base_url()?>instalador/codefactlite0.1.rar" class="list-group-item">
 			<h4 class="list-group-item-heading"><span class="glyphicon glyphicon-save"></span> Codefac Lite 0.1</h4>
 			<p class="list-group-item-text">Compatible con Windows - 2017-12-01</p>
+		</a>
+	</div>
+	<div class="list-group">
+		<a href="<?=base_url()?>instalador/avispa.png" class="list-group-item">
+			<h4 class="list-group-item-heading"><span class="glyphicon glyphicon-save"></span> Codefac Lite 1.2.4</h4>
+			<p class="list-group-item-text">Compatible con Windows  - 2018-07-03</p>
 		</a>
 	</div>
 
