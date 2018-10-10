044dad4608430a89c35c80ccfb717a8633eb9204
carlosmast23 <carlosmast2301@hotmail.es>
2018-10-10 18:06:26 -0500

    Subida version del instaldor y actualizacion de la version 1.2.6.9.2

--- application/views/principal/index.php
+++ application/views/principal/index.php
@@ -14,7 +14,7 @@
       <li>Ahorro de tiempo y errores con automatizaciones de procesos.</li>
     </ul>
 
-    <a class="btn btn-primary btn-block" href="<?=base_url()?>instalador/codefactlite1.2.5.exe"> <span class="glyphicon glyphicon-save"></span> Descargue CODEFAC-LITE!</a>
+    <a class="btn btn-primary btn-block" href="<?=base_url()?>instalador/codefac_setup1.2.6.exe"> <span class="glyphicon glyphicon-save"></span> Descargue CODEFAC-LITE!</a>
     <a class="btn btn-default btn-xs btn-block " href="<?=base_url()?>instalador/InstalaciónConfiguraciónCodefac.pdf"> 
       <span class="glyphicon glyphicon-file"></span> 
       Manual Instalación
